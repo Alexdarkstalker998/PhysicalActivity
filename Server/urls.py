3fc1b614cbb3d52037e28c5d1b6bbbfccad96236
Alexdarkstalker998 <aleksand-nefyedo@yandex.ru>
2019-02-13 15:54:49 +0200

    postgersql

--- Server/urls.py
+++ Server/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from sport import views
+from sportApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- sportApp/models.py
+++ sportApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class sport(models.Model):
-    idKey = models.CharField(max_length=10)
+    idk = models.CharField(max_length=10)
     name = models.CharField(max_length=30)
     photo = models.ImageField()
-    Desc = models.TextField()
+    desc = models.TextField()
