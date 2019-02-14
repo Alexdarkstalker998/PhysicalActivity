6bc664f883d3589c7d0146b12ba8179beca47a7f
Alexdarkstalker998 <aleksand-nefyedo@yandex.ru>
2019-02-14 14:42:04 +0200

    asdsssssswddwkjjjsajld.

--- sportApp/views.py
+++ sportApp/views.py
@@ -84,6 +84,7 @@ def test(request):
     type = us.type2
     l2 = lesson.objects.create(sport = s, coach = t2, lvl="1",wday="Понедельник",tday="11:40",place =p, countmax = '50',countnow='49')
     l2.stud.add(t1)
+    
 
     courses = list(lesson.objects.filter(stud = type))
     an = list()
